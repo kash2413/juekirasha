7b1f21fa3aee68dafd6fe017d14514b0890158e6
kash2413 <kash2413@gmail.com>
2021-05-14 17:46:13 +0400

    1

---
+++ js/br.webfont.config.js
@@ -0,0 +1,6 @@
+WebFont.load({
+    custom: {
+        families: ['open_sansregular', 'open_sanslight', 'open_sansbold', 'open_sanssemibold'],
+        urls: ['/static/css/br_webfont.css?version=1']
+    }
+});
---
+++ js/brfernsehen.webfont.config.js
@@ -0,0 +1,6 @@
+WebFont.load({
+    custom: {
+        families: ['Bayern'],
+        urls: ['/static/css/bereich/fernsehen/br-fernsehen/brfernsehen_webfont.css?version=8']
+    }
+});
